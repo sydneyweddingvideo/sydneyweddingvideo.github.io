bc2077f292012d1b9fd34b27f6c37179f9aa6849
System Administrator <root@MoltenReact.local>
2020-10-04 19:29:34 +1100

    Updates

---
+++ precache-manifest.6b2091295203aeea8d404d3c450512b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12aed5fa0fccd48eb8a8dc82ac99e7fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d939c3bee56c87e72da",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98c1f3210b20b1485a32",
+    "url": "/static/js/2.ce4af907.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce4af907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d939c3bee56c87e72da",
+    "url": "/static/js/main.7dee7735.chunk.js"
+  },
+  {
+    "revision": "d48438c19eabd6c358ae",
+    "url": "/static/js/runtime-main.fbd69d4a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
